699b01dc38d1fa3561d10b40baa11fb831fb0d4e
Charles L <charles.lobo@gmail.com>
2020-09-08 15:53:48 +1000

    feat: add skeleton of program

--- kaf.go
+++ kaf.go
@@ -1,4 +1,49 @@
 package main
 
+import (
+	"fmt"
+)
+
+/*    understand/
+ * main entry point into our program
+ * (It all starts here)
+ *
+ *    way/
+ * Get the user configuration, load existing log files, and start the
+ * server
+ */
 func main() {
+	cfg := getConfig()
+	if cfg == nil {
+		showHelp()
+		return
+	}
+
+	logsChan := loadExistingLogs(cfg.dbloc)
+	startServer(cfg, logsChan)
+}
+
+func getConfig() *config {
+	return nil
+}
+
+func showHelp() {
+	fmt.Println("kaf: Simple Event Store")
+	fmt.Println("go run kaf <port> <path to data folder>")
+}
+
+func loadExistingLogs(dbloc string) chan logReq {
+	return nil
+}
+
+func startServer(cfg *config, logsChan chan logReq) {
+}
+
+type config struct {
+	addr  string
+	dbloc string
+}
+
+type logReq struct {
+	name string
 }
